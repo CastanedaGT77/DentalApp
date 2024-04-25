9c5de9a985ba3a00cb74416b3f23040dbb140b62
null <null>
2024-04-25 09:11:25 -0600

    ss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      'primary': '#0284c7',
+      'secundary': '#5b21b6',
+      'whiteLetter': '#f1f5f9',
+      'blackLetter': '#020617',
+    },
     extend: {},
   },
   plugins: [],
