d5c7752859f1d1433cc6855c6055c1d273838221
CastanedaGT77 <castanedagt77@gmail.com>
2024-07-17 12:06:18 -0600

    citas de back en calendario

--- src/app/axios/axiosConfig.ts
+++ src/app/axios/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const axiosClient = axios.create({
     // baseURL: 'http://localhost:3000/api/',
-    baseURL: 'http://137.184.192.120:3005/api',
+    baseURL: 'https://castaguate.duckdns.org/api',
 
     timeout: 5000
 });
--- src/app/pages/date/date.service.ts
+++ src/app/pages/date/date.service.ts
@@ -1,83 +1,20 @@
 import { Injectable } from "@angular/core";
 import { CitaModel } from "./models/CitaExample";
+import { axiosClient } from "src/app/axios/axiosConfig";
 
 
 
 @Injectable()
 export class DateService {
 
-    async getAppointment() : Promise<CitaModel[]> {
-        try {
-            const appointments: CitaModel[] = [
-                {
-                    id: 1,
-                    patient: "Edvin",
-                    description: "Limpieza",
-                    appointmentDate: "14/07/2024",
-                    hour: "14:30",
-                    hourF: "15:00"
-                },
-                {
-                    id: 1,
-                    patient: "Marco",
-                    description: "Sin Muelas",
-                    appointmentDate: "15/07/2024",
-                    hour: "14:05",
-                    hourF: "14:30"
-                },
-                {
-                    id: 1,
-                    patient: "Meli",
-                    description: "Sin el Palido",
-                    appointmentDate: "14/07/2024",
-                    hour: "15:00",
-                    hourF: "15:50"
-                },
-                {
-                    id: 1,
-                    patient: "Meli",
-                    description: "Sin el Palido",
-                    appointmentDate: "14/07/2024",
-                    hour: "15:00",
-                    hourF: "15:50"
-                },
-                {
-                    id: 1,
-                    patient: "Meli",
-                    description: "Sin el Palido",
-                    appointmentDate: "14/07/2024",
-                    hour: "15:00",
-                    hourF: "15:50"
-                },
-                {
-                    id: 1,
-                    patient: "Meli",
-                    description: "Sin el Palido",
-                    appointmentDate: "14/07/2024",
-                    hour: "15:00",
-                    hourF: "15:50"
-                },
-                {
-                    id: 1,
-                    patient: "Meli",
-                    description: "Sin el Palido",
-                    appointmentDate: "14/07/2024",
-                    hour: "15:00",
-                    hourF: "15:50"
-                },
-                {
-                    id: 1,
-                    patient: "Meli",
-                    description: "Sin el Palido",
-                    appointmentDate: "14/07/2024",
-                    hour: "15:00",
-                    hourF: "15:50"
-                }
-            ];
-            return appointments;
-        } catch(error){
-            return [];
-        }
+  async getAppointment(): Promise<CitaModel[]> {
+    try {
+      axiosClient.defaults.headers.common['Authorization'] = "Bearer 1031283sdasdsa";
+      const response = await axiosClient.get('/appointment');
+      return response.data.data; // Accede a la propiedad 'data'
+    } catch (error) {
+      console.error("Error al obtener citas:", error);
+      return [];
     }
-
+  }
 }
