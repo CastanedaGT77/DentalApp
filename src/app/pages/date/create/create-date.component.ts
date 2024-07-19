654b42c6661fb6addd199b74cec13df7e6df08e0
CastanedaGT77 <castanedagt77@gmail.com>
2024-07-19 15:05:15 -0600

    revisar observations al actualizar cita y cargar bien la fecha al picker

--- src/app/pages/date/create/create-date.component.ts
+++ src/app/pages/date/create/create-date.component.ts
@@ -205,7 +205,7 @@ export class CreateDateComponent implements OnInit {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-    const formattedDate = `${day}-${month}-${year}`;
+    const formattedDate = `${day}/${month}/${year}`;
     this.form.controls["appointmentDate"].setValue(formattedDate);
     this.form.controls["observations"].setValue(this.appointment.observations);
     this.form.controls["startHour"].setValue(this.appointment.startHour);
