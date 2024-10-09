a9a0cd2bcd60c6048375cbb49b8231bf88d4c964
CastanedaGT77 <castanedagt77@gmail.com>
2024-10-09 08:41:06 -0600

    todo bien

--- src/app/axios/axiosConfig.ts
+++ src/app/axios/axiosConfig.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { Router } from '@angular/router'; // Necesitamos inyectar el Router
 
 export const axiosClient = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    //baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://castaguate.duckdns.org/api',
     timeout: 5000
 });
 
