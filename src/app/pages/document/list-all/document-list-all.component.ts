7f3cd1a907605a67bd3e7762f95d47b7650b91e4
CastanedaGT77 <castanedagt77@gmail.com>
2024-10-24 10:10:31 -0600

    CAMBIOS A TABLA DE ARCHIVOS, YA FUNCIONA GET ALL DOCUMENTS

--- src/app/pages/document/list-all/document-list-all.component.ts
+++ src/app/pages/document/list-all/document-list-all.component.ts
@@ -13,13 +13,13 @@ import { DocumentService } from "../document.service";
     templateUrl: "./document-list-all.component.html"
 })
 export class DocumentListAll implements OnInit,AfterViewInit{
-    documents = [];
+    documents: any[] = [];
     dataSource = new MatTableDataSource<any>(this.documents);
     @ViewChild(MatPaginator) paginator: MatPaginator;
 
     crearDocumento: Array<EPermissions>;
 
-    displayedColumns: string[] = ['id', 'name', 'isActive', 'actions'];
+    displayedColumns: string[] = ['id', 'fileName', 'fileCode', 'uploadedBy', 'created_at', 'actions'];
     
     constructor(
         private readonly _router: Router,
@@ -38,7 +38,20 @@ export class DocumentListAll implements OnInit,AfterViewInit{
     }
 
     async getDocuments() {
-       
+        try {
+          const response = await this._documentService.getAllDocuments();
+          if (response && Array.isArray(response)) {
+            this.documents = response;
+            console.log('Documentos1:', this.documents);  // Verifica la estructura aquí
+
+            this.dataSource.data = this.documents; // Para el componente de archivos
+            console.log('Documentos2:', this.dataSource.data);  // Verifica la estructura aquí
+          } else {
+            console.error('Error: No se encontraron datos en la respuesta o no es un array.');
+          }
+        } catch (error) {
+          console.error('Error al obtener datos:', error);
+        }
     }
 
     applyFilter(event: Event) {
@@ -47,9 +60,13 @@ export class DocumentListAll implements OnInit,AfterViewInit{
     }
   
     ngAfterViewInit() {
-        this.dataSource.paginator = this.paginator;
+        if (this.paginator) {
+          this.dataSource.paginator = this.paginator;
+        } else {
+          console.error('Paginator no encontrado');
+        }
     }
-
+      
     redirectCreate(){
         this._router.navigateByUrl("/document/create");
     }
