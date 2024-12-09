8f7ee59d0241c30e7cc2df8f50b9793028d4184b
CastanedaGT77 <castanedagt77@gmail.com>
2024-12-09 11:26:07 -0600

    cambio de logo

--- src/app/layouts/full/sidebar/branding.component.ts
+++ src/app/layouts/full/sidebar/branding.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-branding',
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
     <div class="branding">
       <a href="/">
         <img
-          src="./assets/images/logos/dark-logo.svg"
+          *ngIf="logo"
+          [src]="logo"
           class="align-middle m-2 rounded-lg opacity-2"
           alt="logo"
         />
@@ -14,6 +15,17 @@ import { Component } from '@angular/core';
     </div>
   `,
 })
-export class BrandingComponent {
+export class BrandingComponent implements OnInit {
+  logo: string | null = null;
+
   constructor() {}
+
+  ngOnInit() {
+    // Obtiene el objeto `properties` del localStorage
+    const properties = JSON.parse(localStorage.getItem('properties') || '{}');
+    const config = Array.isArray(properties) ? properties[0] : properties;
+
+    // Asigna la URL del logo si existe
+    this.logo = config.logo || null;
+  }
 }
